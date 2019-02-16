c942352aef5af2c3ddf750ba9684164818430039
Devon McClure <devon.r.mcclure@gmail.com>
2019-02-15 16:30:21 -0800

    remove middleware for login as its globally applied, fix staff and customer routes

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,5 @@
 <?php
-
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::group(['middleware' => ['cors']], function () {
-	Route::post('/login', 'Api\AuthController@login');
-});
+Route::post('/login', 'Api\AuthController@login');
 
 
 Route::group(['middleware' => ['auth:api']], function () {
@@ -32,12 +12,16 @@ Route::group(['middleware' => ['auth:api']], function () {
 	Route::apiResource('categories', 'Api\CategoryController')->only([
 		'index', 'show'
 	]);
-		
+
 	Route::apiResource('products', 'Api\ProductController')->only([
 		'index', 'show'
 	]);
 
 	Route::apiResource('staff', 'Api\StaffController')->only([
-		'index', 'show'
+		'index', 'show', 'update'
+	]);
+
+	Route::apiResource('customers', 'Api\CustomerController')->only([
+		'index', 'show', 'store', 'update'
 	]);
 });
