dc9b8a67300909faadd8a8dc2423c29107d50ecb
Devon McClure <devon.r.mcclure@gmail.com>
2019-02-15 16:30:43 -0800

    create factory to fake customers for testing

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Customer::class, function (Faker $faker) {
+	return [
+		'name' => $faker->name,
+		'phone' => $faker->phoneNumber,
+		'address' => $faker->address,
+		'city' => $faker->city,
+		'province' => $faker->name,
+		'country' => $faker->country,
+		'email' => $faker->email,
+		'location_id' => rand(1, 10) > 5 ? 1 : 2
+	];
+});
